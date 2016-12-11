fea26cbe0c8d1405e9c9d8d4db7341f534b29773
Dane Ketner <daneketner@gmail.com>
2016-12-10 22:19:24 -0900

    Simplify SEMS Path

--- sems_path.m
+++ sems_path.m
@@ -1,28 +1,22 @@
 function sems_path
 
-%SEMS_PATH: Sets path for m-files used by 'sems_gui' and 'sems_program'
+%SEMS_PATH: Sets path for SEMS
 %
 %INPUTS: none
 %
 %OUTPUTS: none
 
 % Author: Dane Ketner, Alaska Volcano Observatory
-% $Date$
-% $Revision$
+% Date: 12/10/2016
+% Revision: 201
 
 addpath(genpath('C:\Github\GISMO-1'));
 addpath(genpath('C:\Github\SEMS'));
 
-
-
 javaaddpath({
-    'C:\Winston1.1\lib\colt.jar', ...
-    'C:\Winston1.1\lib\mysql.jar', ...
-    'C:\Winston1.1\lib\winston-bin.jar'});
+    'C:\Github\Winston1.2\lib\colt.jar', ...
+    'C:\Github\Winston1.2\lib\mysql.jar', ...
+    'C:\Github\Winston1.2\lib\winston-bin.jar'});
 
 format compact
-try
-    cd('C:\AVO\SEMS_SVN\SEMS')
-catch
-    cd('C:\AVO\SEMS')
-end
+
